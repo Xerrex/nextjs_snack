ab1b93ca3faf80cc88c3629f73028602afb04a3f
Alex Kagai <alexkagai@outlook.com>
2024-12-11 10:52:42 +0300

    Enable PPR

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,9 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
  
+
+// export const experimental_ppr = true; //NOTE: fro enabling PPR
+
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
